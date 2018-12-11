0067181b3d5158f6f8b8966dd811581427432d55
Ben Homan <ben@benhoman>
2018-12-11 12:53:00 -0500

    Added in code to check current ip against current cloudflare ip and skip record update if ip address is the same.

--- ddns.py
+++ ddns.py
@@ -9,33 +9,44 @@ def get_ip():
     return str(response.text)
 
 
-def set_ip(current_ip):
+def get_cloudflare_ip(url, headers):
+    response = requests.get(url, headers=headers)
+    response_data = response.json()
+    return str(response_data["result"]["content"])
+
+
+def set_cloudflare_ip(url, headers, current_ip):
+    payload = {
+            'type': 'A',
+            'name': record_name,
+            'content': current_ip
+            }
+    response = requests.put(URL, headers=headers, data=json.dumps(payload))
+    print(response.status_code)
+
+
+def do_ddns():
     zone_id = os.environ.get('ZONE_ID')
     record_id = os.environ.get('RECORD_ID')
-    url = 'https://api.cloudflare.com/client/v4/zones/%(zone_id)s/dns_records/%(record_id)s' % {'zone_id': zone_id, 'record_id': record_id}
-
     api_key = os.environ.get('API_KEY')
     user_email = os.environ.get('USER_EMAIL')
     record_name = os.environ.get('RECORD_NAME')
-
+    url = 'https://api.cloudflare.com/client/v4/zones/%(zone_id)s/dns_records/%(record_id)s' % {'zone_id': zone_id, 'record_id': record_id}
     headers = {
             'X-Auth-Email': user_email,
             'X-Auth-Key': api_key,
             'Content-Type': 'application/json'
             }
 
-    payload = {
-            'type': 'A',
-            'name': record_name,
-            'content': current_ip
-            }
-    response = requests.put(url, headers=headers, data=json.dumps(payload))
-    print(response.status_code)
-
-
-def do_ddns():
     current_ip = get_ip()
-    set_ip(current_ip)
+    cloudflare_ip = get_cloudflare_ip(url, headers)
+    print(current_ip)
+    print(cloudflare_ip)
+    if current_ip == cloudflare_ip:
+        print("IP has not changed. Doing Nothing")
+    else:
+        print("IP has changed. Updating IP to: %(current_ip)s" % {'current_ip': current_ip})
+        set_cloudflare_ip(url, headers, current_ip)
 
 
 if __name__ == '__main__':
